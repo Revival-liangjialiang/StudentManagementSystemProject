3a69e2714351ebf500b98bd55b262ba779fae809
Tony <190174222@qq.com>
2016-04-28 01:20:37 +0800

    完成添加布局

---
+++ src/com/example/studentmanagementsystemproject/activity/ModifyActivity.java
@@ -0,0 +1,12 @@
+package com.example.studentmanagementsystemproject.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ModifyActivity extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView();
+}
+}
